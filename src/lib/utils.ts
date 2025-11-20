ea909b167cd30919288f3f78499d3bf5e8a6a82f
mehmetasl <meas3929@gmail.com>
2025-11-20 17:15:09 +0300

    fixed, slugify added back to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// --- SİLİNEN FONKSİYONU GERİ EKLİYORUZ ---
+export function slugify(text: string) {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')     // Boşlukları tire ile değiştir
+    .replace(/[^\w\-]+/g, '') // Alfanümerik olmayan karakterleri sil
+    .replace(/\-\-+/g, '-')   // Çoklu tireleri tek tire yap
+    .replace(/^-+/, '')       // Baştaki tireleri sil
+    .replace(/-+$/, '')       // Sondaki tireleri sil
+}
